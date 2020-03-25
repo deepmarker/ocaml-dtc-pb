44a91fc10c06ca91aa6dde530e00e4897dfc2070
Vincent Bernardoff <vb@luminar.eu.org>
2020-03-25 15:58:20 +0100

    ocamlformat

--- gen/encoding.mli
+++ gen/encoding.mli
@@ -1,21 +1,13 @@
-type encoding =
-  | Binary
-  | Binary_VLS
-  | Json
-  | Json_compact
-  | Protobuf
+type encoding = Binary | Binary_VLS | Json | Json_compact | Protobuf
 
-type meta = {
-  version : int ;
-  encoding : encoding ;
-}
+type meta = { version : int; encoding : encoding }
 
-type t =
-  | Request of meta
-  | Response of meta
+type t = Request of meta | Response of meta
 
 val read : string -> t option
+
 val read_exn : string -> t
 
 val write : Bytes.t -> t -> unit
+
 val to_string : t -> string
